5c2c3e953b75b8fbbd84f65e860c24367cba69bf
Zefirka <161130791+DaoDeDzin123@users.noreply.github.com>
2025-05-12 18:11:27 +0700

    Update instructions.py

--- instructions.py
+++ instructions.py
@@ -1,1 +1,25 @@
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
 
+txt_hello = 'Приветствую'
+txt_next = 'Дальше'
+txt_instruction = 'Это приложение создано для анализа вашего состояния согласно тесту Руфье и вынесения рекомендаций, соответствующих результату' 
+txt_title = 'Здоровье'
+txt_name = ''
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = ''
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''
